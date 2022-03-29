0cfb5510721e2a064f7cfb0814ea817e8a1837cd
Samaiwolf <matiaslobos12@hotmail.com>
2022-03-29 15:54:57 -0300

    commit mejorado

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # js-rushEnd
+
+...
+
+## Practica de js
+
+
+- Pasos a seguir
+- 1: aprender a escribir codigo .JS
---
+++ figuras.js
@@ -0,0 +1,25 @@
+//Códico del cuadrado
+console.group("consolaCuadrado")
+
+const ladoCuadrado = 5;
+console.log("los lados del cuadrado miden " + ladoCuadrado + " cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("el perimetro del cuadrado es " + perimetroCuadrado + " cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("el area del cuadrado es " + areaCuadrado + " cm2");
+
+console.groupEnd();
+
+//Código del triangulo
+console.group("consoleTriangulo")
+
+const ladoTriangulo1 = 5;
+const ladoTriangulo2 = 5;
+const basoTriangulo = 8;
+
+console.log("los lados del triangulo son 3 lados y el lado 1 es = "+ ladoTriangulo1)
+console.log("lado 2 es = " + ladoTriangulo2)
+console.log("y la base del triangulo es " + basoTriangulo)
+console.groupEnd();
